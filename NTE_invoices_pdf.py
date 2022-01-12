b83a67049ffbad967f5b0627e6530edc5c40e489
Jack Murphy <jackfrancismurphy@outlook.com>
2022-01-12 18:09:16 +0000

    added date and invoice number function

--- NTE_invoices_pdf.py
+++ NTE_invoices_pdf.py
@@ -1,20 +1,37 @@
 from fpdf import FPDF
+from datetime import date
 
 #variables
+
 total = 0
 total_dates = 0
 date_count = 0
 cost = 26.73
 GS_bonus = 5 
-YM_bonus = 9
+YM_bonus = 11
 SCM_bonus = 8
-BP_bonus = 7 
+BP_bonus = 7
+date = date.today() 
 
 GS_dates = input("Which dates for George Street?") 
 YM_dates = input("Which dates for Yorke Mead?") 
 SCM_dates = input("Which dates for St Cuthbert Mayne?")
 BP_dates = input("Which dates for St Bridgewater Primary?")
-invoice_num = input("What number invoice is this?")
+
+
+#external data variable
+
+def get_var_value(filename="invoice_number.txt"):
+    with open(filename, "a+") as f:
+        f.seek(0)
+        val = int(f.read() or 0) + 1
+        f.seek(0)
+        f.truncate()
+        f.write(str(val))
+        return val
+
+invoice_num = get_var_value()
+
 
 total_dates = GS_dates + ' ' + YM_dates + ' ' + SCM_dates + ' ' + BP_dates
 
@@ -45,8 +62,8 @@ pdf.set_text_color(0,0,0)
 # border (0 False; 1 True - add border around cell)
 
 
-pdf.cell(120, 10, f'Invoice {invoice_num}')
-pdf.cell(40, 10, 'Date:[coming soon]', ln=True)
+pdf.cell(140, 10, f'Invoice {invoice_num}')
+pdf.cell(40, 10, f'Date: {date}', ln=True)
 pdf.cell(0, 100, '', ln=True, border=0)
 pdf.cell(0, 10, f'George Street school: {GS_dates}      £{cost}. Bonus = £{GS_bonus}', ln=True, align ='C')
 pdf.cell(0, 10, f'Yorke Mead Primary: {YM_dates}      £{cost}. Bonus = £{YM_bonus}', ln=True, align ='C')
@@ -57,13 +74,3 @@ pdf.cell(0, 10, f'Total = £{total}', ln=True, align ='R')
 
 
 pdf.output(f'{invoice_num} NTE invoice.pdf')
-
-
-
-#invoice number
-#different schools 
-#calculate table?
-#have code run incase there's a different number of children 
-#around 5 minutes into video 2 learn how to put a header and footer image (to make it look pretty)
-#deal with date in a bit.
-#Need to know how to store invoice number outside of this document
